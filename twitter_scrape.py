e0a5a4ecea6da60548f712d9d269845e3a082767
VladoVladVlaVlV <vvojdanovski@fwparker.org>
2017-03-31 17:46:12 -0500

    commit2

--- twitter_scrape.py
+++ twitter_scrape.py
@@ -5,3 +5,20 @@
 # Use BeautifulSoup and urllib to grab the text contents of the tweets
 # located on the twitter page you chose.  The .text attribute will supply the content of a soup object.
 # Have fun.  Again, nothing explicit. (15pts)
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+url="https://twitter.com/nietzsche?lang=en"
+page = urllib.request.urlopen(url)
+soup=BeautifulSoup(page.read(),"html.parser")
+
+
+data_list=[[y.text.strip() for y in x.findAll("div",{"class" : "js-tweet-text-container"})] for x in soup.findAll("ol")[1].findAll("li")]
+
+for i in range(0,len(data_list),3):
+    if i==15:
+        i+=3
+    print(str(data_list[i]))
+
--- weather_scrape.py
+++ weather_scrape.py
@@ -7,6 +7,14 @@
 # for the user.  You will need to target specific classes or other
 # attributes to pull some parts of the data.
 # (e.g.  Wednesday, March 22: the high temp will be 48 with a low of 35, and a 20% chance of rain). (25pts)
-
+import urllib.request
+from bs4 import BeautifulSoup
 
 url = "https://weather.com/weather/tenday/l/USIL0225:1:US"
+page=urllib.request.urlopen(url)
+soup=BeautifulSoup(page.read(),"html.parser")
+
+headers=[[ y.text.strip() for y in x.findAll("td")] for x in soup.find("table").findAll("tr",{"class":"clickable closed"})]
+
+for i in range(len(headers)):
+     print("On ",headers[i][1],"the weather will be",headers[i][2],"with a high of",headers[i][3][0:2],"and a low of",headers[i][3][2:],"with a precipitation chance of",headers[i][4],"and wind of",headers[i][5],"and a humidity of",headers[i][6])
